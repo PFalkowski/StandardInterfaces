446c114119f61d87f72ca9eeaed7d5bcc7b2a0c3
Piotr Falkowski <falcon777@wp.pl>
2018-04-19 00:30:24 +0200

    Add IConverter

---
+++ StandardInterfaces/IConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StandardInterfaces
+{
+    public interface IConverter<in TFrom, out TTo>
+    {
+        TTo Convert(TFrom input);
+    }
+}
