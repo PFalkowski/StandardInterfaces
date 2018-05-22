3b8ef5ca742f8c39e0a8dec3e0bc935b212e0f6c
Piotr Falkowski <falcon777@wp.pl>
2018-05-23 00:36:26 +0200

    Add IFormatter

---
+++ StandardInterfaces/IFormatter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextFormatting
+{
+    public interface IFormatter<in T>
+    {
+        string Format(T input);
+    }
+}
