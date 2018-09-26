588f8f80ace64bd99e3662a779bc6d56f2094233
Piotr Falkowski <falcon777@wp.pl>
2018-09-26 20:32:25 +0200

    Add builders interfaces

---
+++ StandardInterfaces/IBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StandardInterfaces
+{
+    public interface IBuilder<out T>
+    {
+        void Build();
+        T Entity { get; }
+    }
+}
---
+++ StandardInterfaces/IFluentBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StandardInterfaces
+{
+    public interface IFluentBuilder<out T>
+    {
+        IFluentBuilder<T> Build();
+        T Entity { get; }
+    }
+}
