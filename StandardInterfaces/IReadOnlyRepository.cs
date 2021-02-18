d2b9a07f65435d3a197d7854707a95c05120c8fa
Piotr Falkowski <pfalkowski@sevillemore.com>
2021-02-18 20:25:21 +0100

    Extract IReadOnlyRepository from IRepository. IRepository no longer forces implementation of IDisposable - useful for some cases like in memory repositories, where forcing IDisposable would break Liskov.

---
+++ StandardInterfaces/IReadOnlyRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace StandardInterfaces
+{
+    public interface IReadOnlyRepository<TEntity> where TEntity : class
+    {
+        int Count();
+        int Count(Expression<Func<TEntity, bool>> predicate);
+        TEntity Get(Expression<Func<TEntity, bool>> predicate);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate);
+    }
+}
--- StandardInterfaces/IRepository.cs
+++ StandardInterfaces/IRepository.cs
@@ -4,13 +4,8 @@ using System.Linq.Expressions;
 
 namespace StandardInterfaces
 {
-    public interface IRepository<TEntity> : IDisposable where TEntity : class
+    public interface IRepository<TEntity> : IReadOnlyRepository<TEntity> where TEntity : class
     {
-        int Count();
-        int Count(Expression<Func<TEntity, bool>> predicate);
-        TEntity Get(Expression<Func<TEntity, bool>> predicate);
-        IEnumerable<TEntity> GetAll();
-        IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate);
         void Add(TEntity entity);
         void AddOrUpdate(TEntity entity);
         void AddRange(IEnumerable<TEntity> entities);
